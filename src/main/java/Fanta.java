25d2e4b04f952bc6588e468ca277a94056109fd1
Iscaraca <isaac9503@gmail.com>
2026-01-23 02:01:09 +0800

    Add functionality for Rename, Greet, Exit

---
+++ src/main/java/Fanta.java
@@ -0,0 +1,10 @@
+public class Fanta {
+    private static final String DIVIDER = "====================================================";
+    
+    public static void main(String[] args) {        
+        System.out.println("Hello! I'm Fanta");
+        System.out.println("What can I do for you?");
+        System.out.println(DIVIDER);
+        System.out.println("See you soon!");
+    }
+}
