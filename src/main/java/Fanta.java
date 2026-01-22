dc2a016edb4423d060917964e6bd0322b56c049e
Iscaraca <isaac9503@gmail.com>
2026-01-23 02:08:38 +0800

    Add functionality for Echo

--- src/main/java/Fanta.java
+++ src/main/java/Fanta.java
@@ -1,10 +1,26 @@
 public class Fanta {
-    private static final String DIVIDER = "====================================================";
-    
+    private static final String DIVIDER = "  ====================================================";
+
     public static void main(String[] args) {        
-        System.out.println("Hello! I'm Fanta");
-        System.out.println("What can I do for you?");
+        System.out.println("  Hello! I'm Fanta");
+        System.out.println("  What can I do for you?");
         System.out.println(DIVIDER);
-        System.out.println("See you soon!");
+
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+            if ("bye".equals(input)) {
+                System.out.println(DIVIDER);
+                System.out.println("  See you soon!");
+                System.out.println(DIVIDER);
+                break;
+            }
+
+            System.out.println(DIVIDER);
+            System.out.println("  " + input);
+        }
+
+        scanner.close();
     }
 }
